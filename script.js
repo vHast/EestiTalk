9a3f96dd1b7060a1db1294864afc17dc90b03c0e
Federico Alvarez <fedvhast@gmail.com>
2022-03-15 02:53:59 +0100

    New sections

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const hoverWord = (obj) => {
   let fadedWord = document.getElementById('targetHover');
-  fadedWord.style.color = 'white';
+  fadedWord.style.color = 'black';
 }
 
 const outHoverWord = (obj) => {
   let fadedWord = document.getElementById('targetHover');
-  fadedWord.style.color = 'black';
+  fadedWord.style.color = 'white';
 }
 
 // Now we have to find a way to put this function universally available without duplicating it
