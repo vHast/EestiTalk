ad4041bca3dc2c9d490ee4ce841fe9ca86723ac4
vHast <fedvhast@gmail.com>
2022-03-16 11:30:45 +0100

    Fading on js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const hoverWord = (obj) => {
   let fadedWord = document.getElementById('targetHover');
   fadedWord.style.color = 'black';
+  fadedWord.style.transition = '0.2s ease-in'
 }
 
 const outHoverWord = (obj) => {
   let fadedWord = document.getElementById('targetHover');
   fadedWord.style.color = 'white';
+  fadedWord.style.transition = '0.2s ease-in'
 }
 
 // Now we have to find a way to put this function universally available without duplicating it
